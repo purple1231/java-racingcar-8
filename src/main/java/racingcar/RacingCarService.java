b8d99de373505249436f87b0fcff0e8c54c38bf6
roy1704 <roy1704@naver.com>
2025-10-26 23:34:40 +0900

    feat: 서비스 클래스와 관련된 클래스 연동 추가

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -12,62 +12,26 @@ public class RacingCarService {
     private final RacingCarValidator validator = new RacingCarValidator();
     private final Parse parser = new Parse();
 
-    private final List<List<Integer>> roundPositions = new ArrayList<>();
 
 
-
-
-    public List<Car> playGame(String input, int tryCount){
-
-        List<Car> cars = createCars(input);
-
-        for(int i = 0; i < tryCount; i++){
-            loopRound(cars);
-            saveRoundPositions(cars);
-        }
-        return cars;
-    }
     public List<Car> createCars(String input) {
-
         validator.validateCarName(input);
-        List<Car> cars = parser.parseCars(input);
-        return cars;
+        return parser.parseCars(input);
     }
 
 
 
+    public void playGame(List<Car> cars){
 
-    //라운드 루프 반복
-    private void loopRound(List<Car> cars){
-        for (Car car : cars){
-
-            int count = getRandomCount();
-            car.move(count);
+        for (Car car : cars) {
+            int randomNumber = getRandomCount();
+            car.move(randomNumber);
         }
     }
 
     private int getRandomCount(){
         return Randoms.pickNumberInRange(0, 9);
     }
-
-
-
-
-
-    private void saveRoundPositions(List<Car> cars){
-        List<Integer> positions = new ArrayList<>();
-        for (Car car : cars) {
-            positions.add(car.getPosition());
-        }
-        roundPositions.add(positions);
-    }
-
-    public List<List<Integer>> getRoundPositions(){
-        return roundPositions;
-    }
-
-
-
 
 
     public List<String> findWinners(List<Car> cars){
